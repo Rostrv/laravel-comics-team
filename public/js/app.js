54a0fa7291159247cfe17304a78f1ce774a7aa65
muccidavide <davidemucci0804@gmail.com>
2022-06-27 15:35:32 +0200

    Add dev branch

--- public/js/app.js
+++ public/js/app.js
@@ -27654,8 +27654,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\MAMP\htdocs\LARAVEL\laravel-comics-team\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\MAMP\htdocs\LARAVEL\laravel-comics-team\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\MAMP\htdocs\laravel\laravel-comics-team\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\MAMP\htdocs\laravel\laravel-comics-team\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
