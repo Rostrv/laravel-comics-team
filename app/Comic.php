81889474d681918404568b3af85f5fd930f9c824
emascapa <emascapa@gmail.com>
2022-06-27 17:34:12 +0200

    Add migration and seeder

--- app/Comic.php
+++ app/Comic.php
@@ -4,7 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class Comic extends Model
 {
     protected $fillable = ['title', 'description', 'thumb', 'price', 'series', 'sale_date', 'type'];
+
+    public function serie(): BelongsTo
+    {
+        return $this->belongsTo(Serie::class);
+    }
+
 }
